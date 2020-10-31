a757eb2d3f0cfe64bbf7bc5d8e1d1cfaf6c2db5a
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2020-10-31 11:55:21 +0100

    Added general structure for gift wrapping algorithm.

--- lib/convex_hull.ex
+++ lib/convex_hull.ex
@@ -3,4 +3,8 @@ defmodule ConvexHull do
   Module providing functions for computing a convex hull from a list of points. Each point should be a tuple with as many elements as dimensions.
   Each available convex hull algorithm specifies how many dimensions are supported and the running time in asymptotic notation.
   """
+  # Wrapper for the gift wrapping algorithm that allows users of the package to access it easier.
+  def gift_wrapping(points) do
+    ConvexHull.GiftWrapping.gift_wrapping(points)
+  end
 end
---
+++ lib/convex_hull/gift_wrapping.ex
@@ -0,0 +1,8 @@
+defmodule ConvexHull.GiftWrapping do
+  @moduledoc """
+
+  """
+  def gift_wrapping(points) do
+
+  end
+end
