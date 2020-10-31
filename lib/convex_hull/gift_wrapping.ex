0dccf8133b433fa2b912c27cbeefb2ff4e2de8bb
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2020-10-31 11:55:39 +0100

    Added moduledoc for gift wrapping algorithm.

--- lib/convex_hull/gift_wrapping.ex
+++ lib/convex_hull/gift_wrapping.ex
@@ -1,6 +1,8 @@
 defmodule ConvexHull.GiftWrapping do
   @moduledoc """
-
+  Also known as Jarvis' march after R. A. Jarvis who published it in 1973. Simple but not the most efficient algoritm.
+  The algorithm has a time complixity of O(nh), where n is the number of points and h is the number of points on the convex hull.
+  This algorithm can be used when n is small or when h is expected to be very small with respect to n.
   """
   def gift_wrapping(points) do
 
