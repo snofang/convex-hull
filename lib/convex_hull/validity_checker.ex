4892071b2d96753a1e8625f67d8765ec7328ab21
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2020-10-31 15:53:40 +0100

    Added module for validity checking.

---
+++ lib/convex_hull/validity_checker.ex
@@ -0,0 +1,7 @@
+defmodule ConvexHull.ValidityChecker do
+  @moduledoc """
+  Module providing functions to check the validity of the input to the convex hull algorithms. This module contains general
+  validity checks that are relevant for all algorithms, this includes checking if enough points were given to construct a
+  convex hull, checking if all points have the same dimensions and checking if the points have a valid amount of dimensions.
+  """
+end
