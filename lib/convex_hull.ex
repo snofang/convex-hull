700bd4e56d5f5633b5956331a7580fc2a22cddb4
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2020-11-01 13:25:01 +0100

    Moved graham scan doc over to wrapper function.

--- lib/convex_hull.ex
+++ lib/convex_hull.ex
@@ -3,17 +3,24 @@ defmodule ConvexHull do
   Module providing functions for computing a convex hull from a list of points. Each point should be a tuple with as many elements as dimensions.
   Each available convex hull algorithm specifies how many dimensions are supported and the running time in asymptotic notation.
   """
-  @doc """
-  Simple but not the most efficient algorithm, also known as Jarvis' march.
 
-  Created independently by Chand & Kapur in 1970 and R. A. Jarvis in 1973. The algorithm has a time complixity of O(nh),
-  where n is the number of points and h is the number of points on the convex hull. This algorithm can be used when n is
-  small or when h is expected to be very small with respect to n. The algorithm only supports two-dimensional points.
+  @doc """
+  Gift wrapping has a time complixity of O(nh), where n is the number of points and h is the number of points on the convex hull.
+
+  Simple but not the most efficient algorithm, also known as Jarvis' march. Created independently by Chand & Kapur in
+  1970 and R. A. Jarvis in 1973. This algorithm can be used when n is small or when h is expected to be very small with
+  respect to n. The algorithm only supports two-dimensional points.
   """
   def gift_wrapping(points) do
     ConvexHull.GiftWrapping.gift_wrapping(points)
   end
 
+  @doc """
+  Graham scan has a time complexity of O(n log n), where n is the number of points.
+
+  Slighty more sophisticated, but much more efficient than the gift wrapping algorithm. Named after Ronald Graham,
+  who published the algorithm in 1972. The algorithm only supports two-dimensional points.
+  """
   def graham_scan(points) do
     ConvexHull.GrahamScan.graham_scan(points)
   end
