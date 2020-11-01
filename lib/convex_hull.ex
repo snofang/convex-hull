31e3e46da9fce24fe2121d0aa3064f24f70e846d
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2020-11-01 13:17:50 +0100

    Moved gift wrapping doc over to wrapper function.

--- lib/convex_hull.ex
+++ lib/convex_hull.ex
@@ -3,6 +3,13 @@ defmodule ConvexHull do
   Module providing functions for computing a convex hull from a list of points. Each point should be a tuple with as many elements as dimensions.
   Each available convex hull algorithm specifies how many dimensions are supported and the running time in asymptotic notation.
   """
+  @doc """
+  Simple but not the most efficient algorithm, also known as Jarvis' march.
+
+  Created independently by Chand & Kapur in 1970 and R. A. Jarvis in 1973. The algorithm has a time complixity of O(nh),
+  where n is the number of points and h is the number of points on the convex hull. This algorithm can be used when n is
+  small or when h is expected to be very small with respect to n. The algorithm only supports two-dimensional points.
+  """
   def gift_wrapping(points) do
     ConvexHull.GiftWrapping.gift_wrapping(points)
   end
