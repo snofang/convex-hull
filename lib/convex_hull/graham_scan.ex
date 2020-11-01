12b4a1f7936e6f5827470673744fe54e442d3263
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2020-11-01 21:13:21 +0100

    Implemented function to find the lowest point.

--- lib/convex_hull/graham_scan.ex
+++ lib/convex_hull/graham_scan.ex
@@ -1,6 +1,8 @@
 defmodule ConvexHull.GrahamScan do
   @moduledoc false
 
+  # Points used for testing: points = [{2, 1}, {0, 3}, {11, 9}, {6, 6}, {5, 5}, {7, 1}, {8, 5}, {3, 3}, {3, 7}, {7, 8}, {1, 5}]
+
   def graham_scan(points) do
     try do
       ConvexHull.ValidityChecker.is_valid?(points, 2)
@@ -8,5 +10,12 @@ defmodule ConvexHull.GrahamScan do
     rescue
       e in RuntimeError -> IO.puts("An error occurred: " <> e.message)
     end
+  end
+
+  # Find the point with the lowest y-coordinate. If there are multiple points with the lowest y-coordinate, the one with the lowest x-coordinate is returned.
+  def get_lowest_point(points) do
+    Enum.sort(points, fn {x1, y1}, {x2, y2} ->
+      if y1 == y2, do: x1 < x2, else: y1 < y2
+    end)
   end
 end
