4f29bdad8567685adb36a14c0b464d23a26ed8a8
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2020-11-01 01:02:45 +0100

    Simplified validity checker, still needs work.

--- lib/convex_hull/validity_checker.ex
+++ lib/convex_hull/validity_checker.ex
@@ -6,33 +6,32 @@ defmodule ConvexHull.ValidityChecker do
   A relevant runtime error is raised if any of the validity checks fail.
   """
   def is_valid?(points, max_dimensions \\ nil) do
-    check_number_points(points) and check_consistent_dimensions(points) and check_max_dimensions(points, max_dimensions)
+    check_number_points(points)
+    check_consistent_dimensions(points)
+    check_max_dimensions(points, max_dimensions)
+
+    # Return true if all checks pass without rasing errors.
+    true
   end
 
   # Returns true if there are atleast 3 points (the minimum requirement for creating a convex hull). Raises runtime error if not.
   defp check_number_points(points) do
-    if length(points) > 2, do: true, else: raise "Cannot compute convex hull with less than 3 points."
+    if length(points) < 3, do: raise "Cannot compute convex hull with less than 3 points."
   end
 
   # Returns true if all points have the same number of dimensions. Raises runtime error if not.
   defp check_consistent_dimensions(points) do
     num_dimensions = length(Tuple.to_list(List.first(points)))
 
-    if Enum.all?(points, fn point -> length(Tuple.to_list(point)) == num_dimensions end) do
-      true
-    else
+    if Enum.any?(points, fn point -> length(Tuple.to_list(point)) != num_dimensions end) do
       raise "Inconsistent number of dimensions of the given points."
     end
   end
 
   # Returns true if the number of dimensions of the points does not exceed the max allowed number. Raises runtime error if not.
   defp check_max_dimensions(points, max_dimensions) do
-    if max_dimensions == nil do
-      true
-    else
-      if length(Tuple.to_list(List.first(points))) <= max_dimensions do
-        true
-      else
+    if max_dimensions != nil do
+      if length(Tuple.to_list(List.first(points))) > max_dimensions do
         raise "This algorithm does not allow more than #{max_dimensions} dimensions."
       end
     end
