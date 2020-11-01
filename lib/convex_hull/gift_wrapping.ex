98723434b4a8f827456cbf3df36cbdd94c746875
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2020-11-01 01:26:41 +0100

    Implemented gift wrapping algorithm.

--- lib/convex_hull/gift_wrapping.ex
+++ lib/convex_hull/gift_wrapping.ex
@@ -5,20 +5,34 @@ defmodule ConvexHull.GiftWrapping do
   This algorithm can be used when n is small or when h is expected to be very small with respect to n.
   """
   def gift_wrapping(points) do
-    if ConvexHull.ValidityChecker.is_valid?(points, 2) do
+    try do
+      ConvexHull.ValidityChecker.is_valid?(points, 2)
 
+      leftmost = get_leftmost_point(points)
+      gift_wrapping(points, leftmost, [leftmost])
+    rescue
+      e in RuntimeError -> IO.puts("An error occurred: " <> e.message)
+    end
+  end
+  def gift_wrapping(points, current, convex_hull) do
+    next_point = get_next_point(points, current)
+    # If the next point is equal to the first point of the convex hull we are finished.
+    if next_point == List.first(convex_hull) do
+      convex_hull
+    else
+      gift_wrapping(List.delete(points, next_point), next_point, convex_hull ++ [next_point])
     end
   end
 
   # Find a point that is guaranteed to be in the convex hull.
-  def get_leftmost_point(points) do
+  defp get_leftmost_point(points) do
     Enum.reduce(points, List.first(points), fn {x, y}, {x_acc, y_acc} ->
       if x < x_acc, do: {x, y}, else: {x_acc, y_acc}
     end)
   end
 
-  # Returns the next point on the convex hull.
-  def get_next_point([{x, y} | rest_points], {current_x, current_y}) do
+  # Return the next point on the convex hull.
+  defp get_next_point([{x, y} | rest_points], {current_x, current_y}) do
     Enum.reduce(rest_points, {x, y}, fn {check_x, check_y}, {next_x, next_y} ->
       # Vector from current to what we currently think is the next point in the convex hull.
       v1 = {(current_x - next_x), (current_y - next_y)}
@@ -31,8 +45,8 @@ defmodule ConvexHull.GiftWrapping do
     end)
   end
 
-  # Calculates a simple cross product for two dimensional vectors. Only returns the z value.
-  def cross_product({x_v1, y_v1}, {x_v2, y_v2}) do
+  # Calculate a simple cross product for two dimensional vectors. Only returns the z value.
+  defp cross_product({x_v1, y_v1}, {x_v2, y_v2}) do
     (x_v1 * y_v2) - (x_v2 * y_v1)
   end
 end
