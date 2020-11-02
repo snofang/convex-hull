6375e3c46f5619aa527aafac094f68a63a1740a3
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2020-11-02 22:34:25 +0100

    Added unit testing for valid point set.

--- test/validity_checker_test.exs
+++ test/validity_checker_test.exs
@@ -2,7 +2,9 @@ defmodule ValidityCheckerTest do
   use ExUnit.Case
   doctest ConvexHull.ValidityChecker
 
-  test "greets the world" do
+  test "Checks a valid set of points for validity." do
+    points = [{2, 1}, {0, 3}, {11, 9}, {6, 6}, {5, 5}, {7, 1}, {8, 5}, {3, 3}, {3, 7}]
 
+    assert ConvexHull.ValidityChecker.is_valid?(points, 2)
   end
 end
