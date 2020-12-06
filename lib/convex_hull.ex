31d47b5a4b989a95a5d300c3737630c08515f7ba
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2020-12-06 15:02:11 +0100

    Formatted docs.

--- lib/convex_hull.ex
+++ lib/convex_hull.ex
@@ -1,7 +1,9 @@
 defmodule ConvexHull do
   @moduledoc """
-  Module providing functions for computing a convex hull from a list of points. Each point should be a tuple with as many elements as dimensions.
-  Each available convex hull algorithm specifies how many dimensions are supported and the running time in asymptotic notation.
+  Module providing functions for computing a convex hull from a list of points.
+
+  Each point should be a tuple with as many elements as dimensions. Each available convex hull algorithm specifies
+  how many dimensions are supported and the running time in asymptotic notation.
   """
 
   @doc """
--- lib/convex_hull/validity_checker.ex
+++ lib/convex_hull/validity_checker.ex
@@ -1,9 +1,10 @@
 defmodule ConvexHull.ValidityChecker do
   @moduledoc """
-  Module providing functions to check the validity of the input to the convex hull algorithms. This module contains general
-  validity checks that are relevant for all algorithms, this includes checking if enough points were given to construct a
-  convex hull, checking if all points have the same dimensions and checking if the points have a valid amount of dimensions.
-  A relevant runtime error is raised if any of the validity checks fail.
+  Module providing functions to check the validity of the input to the convex hull algorithms.
+
+  This module contains general validity checks that are relevant for all algorithms, this includes checking if enough points
+  were given to construct a convex hull, checking if all points have the same dimensions and checking if the points have a valid
+  amount of dimensions. A relevant runtime error is raised if any of the validity checks fail.
   """
   def is_valid?(points, max_dimensions \\ nil) do
     check_number_points(points)
