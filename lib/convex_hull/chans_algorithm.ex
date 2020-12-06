5c668289108df416bc4794a8688b579e62a8eef5
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2020-12-06 14:53:14 +0100

    Added valid checker to chans algorithm.

--- lib/convex_hull/chans_algorithm.ex
+++ lib/convex_hull/chans_algorithm.ex
@@ -2,6 +2,16 @@ defmodule ConvexHull.ChansAlgorithm do
   @moduledoc false
 
   def chans_algorithm(points) do
+    try do
+      ConvexHull.ValidityChecker.is_valid?(points, 2)
+
+      chans_algorithm(points, 3)
+    rescue
+      e in RuntimeError -> IO.puts("An error occurred: " <> e.message)
+    end
+  end
+
+  def chans_algorithm(points, convex_hull_size_guess) do
 
   end
 end
