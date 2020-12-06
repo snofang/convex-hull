bea4783e0a722bf53aed69f14fc7bfa2ace4cbd6
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2020-12-06 16:36:58 +0100

    Chunking the initial point into subsets and computing the convex hull of each in parallel.

--- lib/convex_hull/chans_algorithm.ex
+++ lib/convex_hull/chans_algorithm.ex
@@ -12,6 +12,16 @@ defmodule ConvexHull.ChansAlgorithm do
   end
 
   def chans_algorithm(points, convex_hull_size_guess) do
+    # Partition the set of points into subsets with at most 'convex_hull_size_guess' points in each.
+    point_subsets = Enum.chunk_every(points, convex_hull_size_guess)
 
+    # Compute the convex hull of each subset in parallel.
+    tasks = Enum.map(point_subsets, fn point_subset ->
+      Task.async(fn -> ConvexHull.graham_scan(point_subset) end)
+    end)
+
+    # Gathering the results from the tasks and removing metadata.
+    results = Task.yield_many(tasks, :infinity)
+    subsets_convex_hulls = Enum.map(results, fn {_task, {:ok, results}} -> results end)
   end
 end
