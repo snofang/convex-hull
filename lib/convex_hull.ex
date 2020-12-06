4cabbea41eeb8a1c10e460abf460a9e9b3133390
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2020-12-06 15:08:35 +0100

    Added "since" to doc.

--- lib/convex_hull.ex
+++ lib/convex_hull.ex
@@ -5,6 +5,7 @@ defmodule ConvexHull do
   Each point should be a tuple with as many elements as dimensions. Each available convex hull algorithm specifies
   how many dimensions are supported and the running time in asymptotic notation.
   """
+  @moduledoc since: "1.0.0"
 
   @doc """
   Gift wrapping has a time complixity of O(nh), where n is the number of points and h is the number of points on the convex hull.
@@ -13,6 +14,8 @@ defmodule ConvexHull do
   1970 and R. A. Jarvis in 1973. This algorithm can be used when n is small or when h is expected to be very small with
   respect to n. The algorithm only supports two-dimensional points.
   """
+  @doc since: "1.0.0"
+
   def gift_wrapping(points) do
     ConvexHull.GiftWrapping.gift_wrapping(points)
   end
@@ -23,6 +26,7 @@ defmodule ConvexHull do
   Slighty more sophisticated, but much more efficient than the gift wrapping algorithm. Named after Ronald Graham,
   who published the algorithm in 1972. The algorithm only supports two-dimensional points.
   """
+  @doc since: "1.0.0"
   def graham_scan(points) do
     ConvexHull.GrahamScan.graham_scan(points)
   end
@@ -34,6 +38,7 @@ defmodule ConvexHull do
   in which the convex hull subsets are calculated. Named after Timothy M. Chan, who published the algorithm in 1996. The algorithm only supports
   two-dimensional points.
   """
+  @doc since: "1.0.0"
   def chans_algorithm(points) do
     ConvexHull.ChansAlgorithm.chans_algorithm(points)
   end
