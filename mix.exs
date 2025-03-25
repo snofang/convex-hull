a1fb2bce6d88b504ada4b41fb6a59ad82bf6c289
Mehdi Taghi <snofang@gmail.com>
2025-03-25 19:24:19 +0330

    removing remix 4 getting rid of errors and warnings

--- mix.exs
+++ mix.exs
@@ -14,18 +14,18 @@ defmodule ConvexHull.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      applications: applications(Mix.env),
+      # applications: applications(Mix.env()),
       extra_applications: [:logger]
     ]
   end
 
-  defp applications(:dev), do: applications(:all) ++ [:remix]
-  defp applications(_all), do: [:logger]
+  # defp applications(:dev), do: applications(:all) ++ [:remix]
+  # defp applications(_all), do: [:logger]
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:remix, "~> 0.0.1", only: :dev}
+      # {:remix, "~> 0.0.1", only: :dev}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
